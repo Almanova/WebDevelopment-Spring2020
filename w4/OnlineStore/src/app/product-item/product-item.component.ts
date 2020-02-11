3d61026cf667e7c53e4413b3947d25d83faeb8be
Almanova <almanovamadina@yahoo.com>
2020-02-12 03:37:37 +0600

    week4 added

---
+++ w4/OnlineStore/src/app/product-item/product-item.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-product-item',
+  templateUrl: './product-item.component.html',
+  styleUrls: ['./product-item.component.css']
+})
+export class ProductItemComponent implements OnInit {
+  @Input() product;
+  slideIndex = 1;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  share() {
+    this.product.share = 'https://telegram.me/share/url?url=' + this.product.link 
+      + '&text=Hey, check out the ' + this.product.name + '!';
+  }
+
+  onNotify() {
+    window.alert('You will be notified when the product goes on sale');
+  }
+}
