6ff74d2334fe78465e0c866266db7d6c524277d8
Almanova <almanovamadina@yahoo.com>
2020-03-06 14:46:39 +0600

    lab6

---
+++ TourOfHeroes/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Dr Nice', views: 0 },
+    { id: 12, name: 'Narco', views: 0 },
+    { id: 13, name: 'Bombasto', views: 0 },
+    { id: 14, name: 'Celeritas', views: 0 },
+    { id: 15, name: 'Magneta', views: 0 },
+    { id: 16, name: 'RubberMan', views: 0 },
+    { id: 17, name: 'Dynama', views: 0 },
+    { id: 18, name: 'Dr IQ', views: 0 },
+    { id: 19, name: 'Magma', views: 0 },
+    { id: 20, name: 'Tornado', views: 0 }
+]
